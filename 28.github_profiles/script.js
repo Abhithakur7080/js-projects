6a483224142d27e4ce094bfc7ce9b1ad19c57893
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-05-15 17:56:24 +0530

    28 - Github profiles completed

---
+++ 28.github_profiles/script.js
@@ -0,0 +1,77 @@
+const API_URL = 'https://api.github.com/users/'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username){
+    try {
+        const { data } = await axios(API_URL + username)
+        createUserCard(data);
+        getRepos(username)
+    } catch (error) {
+        if(error.response.status === 404){
+            createUserCard('No Profile with this username')
+        }
+    }
+}
+
+async function getRepos(username){
+    try {
+        const { data } = await axios(API_URL + username + '/repos?sort=created')
+        addReposToCard(data)
+    } catch (error) {
+        createErrorCard('Problem fetching repos')
+    }
+}
+function createUserCard(user){
+    const userID = user.name || user.login
+    const userBio = user.bio ? `<p>${user.bio}</p>`:''
+    const cardHTML = `
+    <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${userID}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos"></div>
+    </div>
+  </div>
+    `
+    main.innerHTML = cardHTML
+}
+function createErrorCard(message){
+    const cardHTML = `
+        <div class="card">
+            <h1>${message}</h1>
+        </div>
+    `
+    main.innerHTML = cardHTML
+}
+function addReposToCard(repos){
+    const reposElem = document.getElementById('repos');
+    repos.slice(0, 5).forEach(repo => {
+        const repoElem = document.createElement('a')
+        repoElem.classList.add('repo');
+        repoElem.href = repo.html_url
+        repoElem.target = '_blank'
+        repoElem.innerText = repo.name
+        
+        reposElem.appendChild(repoElem)
+    })
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const user = search.value
+    if(user){
+        getUser(user);
+        search.value = ''
+    }
+})
